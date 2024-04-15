8c64b5e0ca4617a2cca354a25e5dad248bef63a1
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-04-15 19:47:52 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+// import Table from './Table';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<h1>test</h1>
+		<h4
+			className='
+  font-normal
+  text-[red]
+  text-[1.4rem]'>
+			XXXX
+		</h4>
+		{/* <Table /> */}
+	</StrictMode>
+);
