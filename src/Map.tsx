e908c384f0193831bd66637b952120beaf6fe328
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-04-17 20:51:15 +0900

    update: 座席表サンプル画像を地図の代わりに表示、クリックして座標を取得

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, ImageOverlay, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LatLngBounds } from 'leaflet';
 
 const Map = () => {
-	const [position, setPosition] = useState({
-		lat: 51.505,
-		lng: -0.09,
-	});
-	const [zoom, setZoom] = useState(13);
+	const [bounds, setBounds] = useState(
+		new LatLngBounds(
+			[51.515, -0.1], // top left corner
+			[51.495, -0.08] // bottom right corner
+		)
+	);
+
+	const MapClickHandler = () => {
+		const map = useMap();
+		map.on('click', (e) => {
+			console.log(e.latlng); // クリックした座標の位置をコンソールに出力
+			// ここで必要な処理を追加する
+		});
+		return null;
+	};
 
 	return (
 		<div>
-			<MapContainer center={position} zoom={zoom} style={{ height: '50vh' }}>
-				<TileLayer
-					attribution='&amp;copy <a href="http://osm.org/copyright";>OpenStreetMap</a> contributors'
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				/>
-				<Marker position={position}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+			<MapContainer
+				zoomControl={false}
+				scrollWheelZoom={false}
+				doubleClickZoom={false}
+				touchZoom={false}
+				boxZoom={false}
+				dragging={false}
+				center={[51.505, -0.09]}
+				zoom={16}
+				style={{ height: '90vh' }}>
+				<MapClickHandler />
+				<ImageOverlay url='src/assets/Desktop.svg' bounds={bounds} />
 			</MapContainer>
 		</div>
 	);
