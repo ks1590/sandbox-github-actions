9d7ad64d95d24158a430444f6fae8c08ff4bb540
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-04-21 13:47:14 +0900

    update: ドラッグ操作した図形の座標取得

--- src/Map.tsx
+++ src/Map.tsx
@@ -6,38 +6,56 @@ import {
 	TileLayer,
 	Rectangle,
 	useMap,
+	useMapEvent,
 } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { DraggableCircle } from './components/DraggableCircle';
 
-const DraggableRectangle = ({ bounds, options }) => {
+const DraggableRectangle = ({ bounds, options, setBounds }) => {
 	const map = useMap();
 
 	useEffect(() => {
 		const rectangle = L.rectangle(bounds, options).addTo(map);
-		const draggable = new L.Draggable(rectangle.getElement());
+		const draggable = new L.Draggable(rectangle.getElement() as HTMLElement);
 		draggable.enable();
 
 		// ドラッグ終了時に新しい境界をログに出力
 		draggable.on('dragend', function () {
-			console.log(rectangle.getBounds());
+			const bounds = rectangle.getBounds();
+			console.log('🚀 dragend ~ bounds:', bounds);
+
+			const pos = map.layerPointToLatLng(this._newPos);
+			console.log('🚀 ~ this:', this);
+			console.log('🚀 ~ pos:', pos);
+		});
+
+		// ドラッグ終了時に新しい境界をログに出力
+		draggable.on('moveend', () => {
+			const center = rectangle.getCenter();
+			console.log('マウスの位置座標:', center);
 		});
 
 		return () => {
 			draggable.disable();
 			map.removeLayer(rectangle);
 		};
-	}, [map, bounds, options]);
+	}, [map, bounds, options, setBounds]);
 
 	return null;
 };
 
 const Map = () => {
-	const [position, setPosition] = useState({ lat: 55.3781, lng: -3.436 }); // イギリスの中心を初期位置として設定
+	const [position, setPosition] = useState({ lat: 51.505, lng: -0.09 }); // イギリスの中心を初期位置として設定
 	const [zoom, setZoom] = useState(5); // 表示範囲を広げるためにズームレベルを調整
+	const [rectangleBounds, setRectangleBounds] = useState([
+		[54.559322, -5.767822],
+		[56.1210604, -3.02124],
+	]);
 
 	return (
 		<div>
-			<MapContainer center={position} zoom={zoom} style={{ height: '90vh' }}>
+			<MapContainer center={position} zoom={zoom} style={{ height: '100vh' }}>
 				<TileLayer
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 					attribution='&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
@@ -48,12 +66,11 @@ const Map = () => {
 					</Popup>
 				</Marker>
 				<DraggableRectangle
-					bounds={[
-						[54.559322, -5.767822],
-						[56.1210604, -3.02124],
-					]}
+					bounds={rectangleBounds}
 					options={{ color: '#ff7800', weight: 1 }}
+					setBounds={setRectangleBounds}
 				/>
+				<DraggableCircle />
 			</MapContainer>
 		</div>
 	);
